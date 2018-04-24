a4098a8e082741ba1decae8eb8ec68f20b7eba27
dkang417 <dkang417@gmail.com>
2018-04-23 23:21:56 -0400

    update

--- python/python_stack/flask_mysql/EmailValidation/server.py
+++ python/python_stack/flask_mysql/EmailValidation/server.py
@@ -7,21 +7,31 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app.secret_key = "secret"
 
 @app.route('/')
-def index():
-	return render_template('index.html')
+def index():                      
+    return render_template('index.html') 
+
+
+def create(request):
+	query = "INSERT INTO email(email,created_at,updated_at) Values(:email, now(), now())"
+	data = {
+ 			'email': request.form['email']
+		}
+	mysql.query_db(query, data)
+
+def display():
+	query = "SELECT * FROM email"
+	emails = mysql.query_db(query)
+	return render_template('success.html',emails=emails)
+
 
 @app.route('/success', methods=['POST'])
-def create():
+def result():
 	if not EMAIL_REGEX.match(request.form['email']):
 		flash("EMAIL is not valid!")
 		return redirect('/')
 	else:
-		query = "INSERT INTO email(email,created_at,updated_at) Values(:email, now(), now())"
-		data = {
- 			'email': request.form['email']
-			}
-		emails= mysql.query_db(query, data)
-		
-		return render_template('success.html', all_emails=emails)
-	
+		flash("the email you entered is a valid email address. Thank you!")
+		create(request)
+		return display()
+
 app.run(debug=True)
