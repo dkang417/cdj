5d7699c481d9cb7791b59c20dd05adb18c2cf67f
dkang417 <dkang417@gmail.com>
2018-05-04 20:16:49 -0400

    added Session Words app

---
+++ Python/python_stack/Django/Session_words/main/apps/session_words/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse, redirect
+from datetime import datetime
+
+
+def index(request):
+	if request.session.get('words') == None:
+		request.session['words'] = []
+		
+	return render(request,'session_words/index.html')
+
+def process(request):
+	print "success"
+
+	word = request.POST['word']
+	color = request.POST['color']
+
+	if 'big_font' in request.POST:
+		font = "big"
+	else: 
+		font = "normal"
+
+	dateandtime = datetime.strftime(datetime.now(), "%H:%M:%S %p, %B %d, %Y")
+
+	request.session['words'].append({'word': word, 'color':color, 'font': font, 'dateandtime': dateandtime})
+	request.session.modified = True
+	
+	return redirect('/result')
+
+def result(request):	
+	return render(request, 'session_words/success.html')
+
+def clear(request):
+	request.session.clear()
+	return redirect('/')
